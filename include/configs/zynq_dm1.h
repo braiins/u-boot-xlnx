6e565515174861e6af7beb6f2d9b45444c063130
Libor Vašíček <libor.vasicek@braiins.cz>
2018-03-17 15:02:34 +0100

    Rename board name to official DragonMint
    - create separate boards for G9 and G19 version

---
+++ include/configs/zynq_dm1.h
@@ -0,0 +1,17 @@
+/*
+ * (C) Copyright 2013 Xilinx, Inc.
+ *
+ * Configuration settings for the Xilinx Zynq ZC702 and ZC706 boards
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_DM1_H
+#define __CONFIG_ZYNQ_DM1_H
+
+#define CONFIG_SYS_NO_FLASH
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_DM1_H */
